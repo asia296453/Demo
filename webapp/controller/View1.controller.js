4ea51aed267a30000a9f31319cf3aa80236c0958
Asia Taheershaikh <asia.taheershaikh@hitachivantara.com>
2023-10-05 09:55:11 +0000

    commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("demo.controller.View1", {
+            onInit: function () {
+
+            },
+            onButtonPress: function(oEvent) {
+                var oButton = oEvent.getSource();
+                this.byId("actionSheet").openBy(oButton);
+            }
+        });
+    });
